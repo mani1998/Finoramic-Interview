cdab164f31f0403b32f298576349030e5612ba37
mani1998 <31241086+mani1998@users.noreply.github.com>
2020-04-22 21:30:43 +0530

    Update script-object.js

--- script-object.js
+++ script-object.js
@@ -2,7 +2,6 @@ var packages=require('./packages.json')
 const { execSync } = require("child_process");
 packages=packages["dependencies-obj"];
 var keys=Object.keys(packages)
-console.log(keys)
 var failed=[];
 for(let i=0;i<keys.length;++i){
 try{
